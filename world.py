1d5a529fe797b4ec83246b0db7dce5aaa7e1c7ac
Eli Thorpe <elithorpe42@gmail.com>
2018-03-14 17:00:44 -0400

    Adds in a moving object, ticks

--- world.py
+++ world.py
@@ -1,3 +1,4 @@
+import math
 class world(object):
     def __init__(self, x=10, y=10):
         '''Makes a new world object, with dimensions x and y'''
@@ -5,7 +6,9 @@ class world(object):
         self.structures=[]
         self.x=x
         self.y=y
-        self.view=[[""]*x]*y
+        self.view=[]
+        for i in range(y):
+            self.view.append(['']*x)
     def ground(self):
         '''Fills in the grid with blank floors'''
         for i in range(self.x):
@@ -17,11 +20,17 @@ class world(object):
         for floor in self.floors:
             x=floor.x
             y=floor.y
+            if not (0<=x<self.x and 0<=y<self.y):
+                print "Something is out of bounds"
+                continue
             sprite=floor.sprite
             self.view[y][x]=sprite
         for struc in self.structures:
             x=struc.x
             y=struc.y
+            if not (0<=x<self.x and 0<=y<self.y):
+                print "Something is out of bounds"
+                continue
             sprite=struc.sprite
             self.view[y][x]=sprite
         val=""
@@ -46,6 +55,22 @@ class structure(object):
         self.x=x
         self.y=y
         self.sprite=sprite
+    def tick(self):
+        pass
 class rock(structure):
     def __init__(self, x,y):
-        structure.__init__(x,y,"*")
+        structure.__init__(self,x,y,"*")
+class boulder(structure):
+    def __init__(self, x, y, speedx, speedy, friction):
+        structure.__init__(self, x, y, "o")
+        self.dx=speedx
+        self.dy=speedy
+        self.k=friction
+    def tick(self): #needed to think back to physics to write this function
+        self.x+=self.dx
+        self.y+=self.dy
+        thet=math.atan2(self.dy, self.dx)
+        d=math.sqrt(self.dx**2+self.dy**2)
+        d-=1
+        self.dx=int(d*math.cos(thet))
+        self.dy=int(d*math.sin(thet))
