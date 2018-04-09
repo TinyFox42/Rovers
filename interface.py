f6ad4c3880efa4c04b074d488f8ae23822dc8d97
TinyRex42 <elithorpe42@gmail.com>
2018-04-09 14:09:42 -0400

    Some work on text entry
    Now I just need to connect this to the old main (yeah, that should be easy)

--- interface.py
+++ interface.py
@@ -1,12 +1,22 @@
 #Will handel most of the GUI
 from Tkinter import *
+
 class interface(object):
+    
+
     def __init__(self, x, y):        
         self.root=Tk()
         self.c=Canvas(self.root, width=x*10, height=y*10)
         self.c.pack()
         self.c.create_rectangle(0,0,10,10, fill='red')
+        self.v=StringVar()
+        self.l=Entry(self.root, textvariable=self.v)
+        self.l.bind('<Return>', self.stuff)
+        self.l.pack()
         self.root.mainloop()
+    def stuff(self,event):
+        print "Text was " +self.v.get()
     def test_rect(self, x1, y1, x2, y2):
         self.c.create_rectangle(x1,y1, x2, y2, fill='red')
-    
+    def what(self):
+        print self.v.get()
