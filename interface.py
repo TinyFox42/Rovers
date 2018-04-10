98f81330d0e41198c7cb0837aeb256775c624640
Eli Thorpe <elithorpe42@gmail.com>
2018-04-10 15:56:37 -0400

    Some work on the interface
    Need to switch to a grid, so that I have somewhere to print text

--- interface.py
+++ interface.py
@@ -7,12 +7,15 @@ class interface(object):
     def __init__(self, x, y):        
         self.root=Tk()
         self.c=Canvas(self.root, width=x*10, height=y*10)
-        self.c.pack()
+        #self.c.pack()
+        self.c.grid(row=0)
         self.c.create_rectangle(0,0,10,10, fill='red')
         self.v=StringVar()
         self.l=Entry(self.root, textvariable=self.v)
         self.l.bind('<Return>', self.stuff)
-        self.l.pack()
+        #self.l.pack()
+        self.l.grid(row=1)
+        
         self.root.mainloop()
         print "test"
     def stuff(self,event):
