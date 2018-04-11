49af8e9a030e1997f637a9649a84ab308297f208
TinyRex42 <elithorpe42@gmail.com>
2018-04-11 13:35:02 -0400

    Sets up prefixes to the text entry field
    Just need somewhere to print stuff to now

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 #Will handel most of the GUI
 from Tkinter import *
-
+stuff=[]
 class interface(object):
     
 
@@ -11,6 +11,8 @@ class interface(object):
         self.c.grid(row=0)
         self.c.create_rectangle(0,0,10,10, fill='red')
         self.v=StringVar()
+        self.v.trace('w',self.edited)
+        self.start=''
         self.l=Entry(self.root, textvariable=self.v)
         self.l.bind('<Return>', self.stuff)
         #self.l.pack()
@@ -24,3 +26,10 @@ class interface(object):
         self.c.create_rectangle(x1,y1, x2, y2, fill='red')
     def what(self):
         print self.v.get()
+    def edited(self, *args):
+        '''Runs whenever the text is edited, to make sure it starts with the right thing'''
+        if self.v.get().startswith(self.start)==False:#test to see if it starts correctly
+            #print "edited"
+            self.v.set(self.start)#update the string to be the start
+            self.l.icursor(len(self.start))#move the cursor to the end of the start
+            #doesn't work as well as I had hoped
