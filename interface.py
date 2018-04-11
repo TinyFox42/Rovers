3c28a9642a601f9fa4a8744ad90ac822da7d64d3
TinyRex42 <elithorpe42@gmail.com>
2018-04-11 13:58:44 -0400

    Adds in the space to print stuff!
    Now I just need to make it actually be able to be printed to...

--- interface.py
+++ interface.py
@@ -6,20 +6,28 @@ class interface(object):
 
     def __init__(self, x, y):        
         self.root=Tk()
+        #f=Frame(self.root)
         self.c=Canvas(self.root, width=x*10, height=y*10)
+        #self.c=Canvas(f, width=x*10, height=y*10)
         #self.c.pack()
-        self.c.grid(row=0)
         self.c.create_rectangle(0,0,10,10, fill='red')
         self.v=StringVar()
         self.v.trace('w',self.edited)
         self.start=''
-        self.l=Entry(self.root, textvariable=self.v)
+        self.l=Entry(self.root, textvariable=self.v, width=40)
         self.l.bind('<Return>', self.stuff)
         #self.l.pack()
-        self.l.grid(row=1)
-        
+        self.t=Text(self.root, state=DISABLED,height=12, width=40)
+        #self.t=Text(f, state=DISABLED, height=10, width=10)
+        self.c.grid(row=0)
+        self.l.grid(row=1, columnspan=2)
+        self.t.grid(row=0, column=1)
+        #f.pack(expand=True)
+        '''self.c.pack(expand=True, side=LEFT)
+        self.l.pack(expand=True)
+        self.t.pack(expand=True, side=LEFT)'''
         self.root.mainloop()
-        print "test"
+        #print "test"
     def stuff(self,event):
         print "Text was " +self.v.get()
     def test_rect(self, x1, y1, x2, y2):
