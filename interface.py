f79a756c21fd73eeb555f4d156e74bbdd6b82365
TinyRex42 <elithorpe42@gmail.com>
2018-04-11 14:12:13 -0400

    Adds in printing to the box

--- interface.py
+++ interface.py
@@ -18,6 +18,8 @@ class interface(object):
         self.l.bind('<Return>', self.stuff)
         #self.l.pack()
         self.t=Text(self.root, state=DISABLED,height=12, width=40)
+        self.t.mark_set("start", INSERT)
+        self.t.mark_gravity("start", LEFT)
         #self.t=Text(f, state=DISABLED, height=10, width=10)
         self.c.grid(row=0)
         self.l.grid(row=1, columnspan=2)
@@ -30,6 +32,7 @@ class interface(object):
         #print "test"
     def stuff(self,event):
         print "Text was " +self.v.get()
+        self.set_text(self.v.get())
     def test_rect(self, x1, y1, x2, y2):
         self.c.create_rectangle(x1,y1, x2, y2, fill='red')
     def what(self):
@@ -41,3 +44,11 @@ class interface(object):
             self.v.set(self.start)#update the string to be the start
             self.l.icursor(len(self.start))#move the cursor to the end of the start
             #doesn't work as well as I had hoped
+    def set_text(self, text):
+        self.t.config(state=NORMAL)
+        self.t.delete('start', END)
+        self.t.insert(END, text)
+        self.t.config(state=DISABLED)
+    def set_prefix(self, prefix):
+        self.v.set(prefix)
+        self.start=prefix
