862bfd79ace061c030ac011c0bf361e87b4f152b
Eli Thorpe <elithorpe42@gmail.com>
2018-04-26 17:16:13 -0400

    Makes config not break when working directory isn't set

--- config_manager.py
+++ config_manager.py
@@ -1,22 +1,33 @@
 import re
+import os.path
+directory=os.path.abspath(__file__)
+filename=os.path.join(directory, "rover_config.txt")
 config={}
-text=open('rover_config.txt','r')
-lines=text.readlines()
-text.close()
-for line in lines:
-    if line.startswith('#'):
-        continue
-    name=re.findall('.*?(\S+)\s*?=',line)
-    if len(name)!=1:
-        continue
-    name=name[0]
-    value=re.findall('.*=\s?(\S+)',line)
-    if len(value)!=1:
-        continue
-    value=value[0]
-    if value.isdigit():
-        value=int(value)
-    config[name]=value
+try:
+    text=open(filename,'r')
+    lines=text.readlines()
+    text.close()
+    for line in lines:
+        if line.startswith('#'):
+            continue
+        name=re.findall('.*?(\S+)\s*?=',line)
+        if len(name)!=1:
+            continue
+        name=name[0]
+        value=re.findall('.*=\s?(\S+)',line)
+        if len(value)!=1:
+            continue
+        value=value[0]
+        if value.isdigit():
+            value=int(value)
+        config[name]=value
+except IOError:
+    #default backup values
+    print "rovers_config.txt not found, using default values"
+    config['config_test']='N'
+    config['tile_size']=100
+    config['collision_efficiency']=.5
+    config['scale']=10
 if config['config_test']=='Y':
     for name in config.keys():
         print name +'='+ str(config[name])
