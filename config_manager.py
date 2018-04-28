24e0935ff497c81c46d61d468714b58a491ae103
Eli Thorpe <elithorpe42@gmail.com>
2018-04-28 17:33:31 -0400

    Fixes config_manager
    Yeah, I should have thought about it more...

--- config_manager.py
+++ config_manager.py
@@ -1,7 +1,8 @@
 import re
 import os.path
-directory=os.path.abspath(__file__)
+directory=os.path.dirname(os.path.abspath(__file__))
 filename=os.path.join(directory, "rover_config.txt")
+print filename
 config={}
 try:
     text=open(filename,'r')
@@ -23,7 +24,7 @@ try:
         config[name]=value
 except IOError:
     #default backup values
-    print "rovers_config.txt not found, using default values"
+    print "rover_config.txt not found, using default values"
     config['config_test']='N'
     config['tile_size']=100
     config['collision_efficiency']=.5
