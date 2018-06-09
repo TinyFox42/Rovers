f80b9108911ae91408d03d79d6ad030e7d61eb13
Eli Thorpe <elithorpe42@gmail.com>
2018-06-09 14:27:09 -0400

    Finishes up the map rendering

--- java/Rovers/cdVeiw.java
+++ java/Rovers/cdVeiw.java
@@ -29,18 +29,29 @@ public class cdVeiw implements veiwer
     }
     public void draw(ArrayList<obj> obs){
         map="";
-        String [][] map=new String[10][10];//Yeah, for now max map is 10X10
+        String [][] temp=new String[10][10];//Yeah, for now max map is 10X10
         ////Should probably set them all to .s here
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                temp[i][j]=" ";
+            }
+        }
         //For the record, for the GUI I should look into AWT
         //Seems to be like tkinter, but in this case I will be working with java
         for(int i=0; i<obs.size(); i++){
             obj t=obs.get(i);
             if(0<=t.get_x()&&t.get_x()<=9&&0<=t.get_y()&&t.get_y()<=9){
-                map[t.get_y()][t.get_x()]=t.draw();
+                temp[t.get_y()][t.get_x()]=t.draw();
             }
             else{
                 System.out.println("Something is out of bounds");
             }
+        }
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                map+=temp[i][j];
+            }
+            map+="\n";
         }
     }
 }
